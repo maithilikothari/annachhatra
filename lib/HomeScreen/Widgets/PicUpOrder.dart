d501a7452a5e856fd65a919c458671dd48456f1b
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2022-05-20 12:39:51 +0530

    ViewDonation updated

--- lib/HomeScreen/Widgets/PicUpOrder.dart
+++ lib/HomeScreen/Widgets/PicUpOrder.dart
@@ -9,142 +9,149 @@ class PickUPOrder extends StatefulWidget {
 class _PickUPOrderState extends State<PickUPOrder> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-      child: Material(
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ),
-        elevation: 8,
-        child: Container(
-          height: MediaQuery.of(context).size.height -
-              MediaQuery.of(context).padding.top -
-              AppBar().preferredSize.height * 12,
-          width: double.maxFinite,
-          decoration: BoxDecoration(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View Donation'),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+          child: Material(
             borderRadius: BorderRadius.all(
               Radius.circular(20),
             ),
-            color: Colors.white,
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 3,
-                child: Container(
-                  padding: EdgeInsets.only(top: 40, left: 10, right: 20),
-                  height: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          bottomLeft: Radius.circular(20)),
-                      color: Colors.lightGreen.shade200),
-                  child: Column(children: const [
-                    (Text(
-                      'Quantity',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    )),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      '2',
-                      style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ]),
+            elevation: 8,
+            child: Container(
+              height: MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top -
+                  AppBar().preferredSize.height * 12,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
                 ),
+                color: Colors.white,
               ),
-              Expanded(
-                flex: 5,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 40, left: 10, right: 20),
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              bottomLeft: Radius.circular(20)),
+                          color: Colors.lightGreen.shade200),
+                      child: Column(children: const [
+                        (Text(
+                          'Quantity',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          '2',
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Text(
-                            'Paneer Masala',
-                            style: TextStyle(color: Colors.green),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Paneer Masala',
+                                style: TextStyle(color: Colors.green),
+                              ),
+                              SizedBox(
+                                width: 40,
+                              ),
+                              Text(
+                                '2',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              )
+                            ],
                           ),
                           SizedBox(
-                            width: 40,
+                            height: 17,
                           ),
-                          Text(
-                            '2',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 17,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Chicken Kebab',
-                            style: TextStyle(color: Colors.deepOrange),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Chicken Kebab',
+                                style: TextStyle(color: Colors.deepOrange),
+                              ),
+                              SizedBox(
+                                width: 40,
+                              ),
+                              Text(
+                                '10',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              SizedBox(height: 70),
+                            ],
                           ),
-                          SizedBox(
-                            width: 40,
-                          ),
-                          Text(
-                            '10',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(height: 70),
-                        ],
-                      ),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              '9.30pm,Today',
-                              style: TextStyle(
-                                  fontSize: 13, fontStyle: FontStyle.italic),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                decoration: BoxDecoration(),
-                                height: 25,
-                                child: RawMaterialButton(
-                                  shape: StadiumBorder(),
-                                  fillColor: Colors.red,
-                                  onPressed: () {},
-                                  child: Text(
-                                    'Delete',
-                                    style: TextStyle(
-                                      color: Colors.white,
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  '9.30pm,Today',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Container(
+                                    decoration: BoxDecoration(),
+                                    height: 25,
+                                    child: RawMaterialButton(
+                                      shape: StadiumBorder(),
+                                      fillColor: Colors.red,
+                                      onPressed: () {},
+                                      child: Text(
+                                        'Delete',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ]),
-                    ],
+                              ]),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
